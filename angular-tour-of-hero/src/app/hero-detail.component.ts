fc01792cfe6b63acebffcdbf46885cfd9a54c872
zicong <zicong@igola.com>
2016-12-05 23:33:28 +0800

    Hero--使用服务和路由

--- angular-tour-of-hero/src/app/hero-detail.component.ts
+++ angular-tour-of-hero/src/app/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import {  Location } from '@angular/common';
+
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
+import 'rxjs/add/operator/switchMap'
 
 @Component({
     selector: 'my-hero-detail',
@@ -12,10 +18,30 @@ import { Hero } from './hero';
                 <input [(ngModel)]="hero.name" placeholder="name">
             </div>
         </div>
+        <button (click)="goBack()">Back</button>
+
     `
 })
 
-export class HeroDetailComponent{
+export class HeroDetailComponent implements OnInit{
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ){}
+
     @Input()
     hero: Hero;
+
+    ngOnInit(): void{
+        this.route.params
+            .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+            .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void{
+        this.location.back();
+    }
+
 }
